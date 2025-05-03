a9e3a168277e9303fd80f11c92148eedabcd435c
Nasim <mdnasimhosen333@gmail.com>
2025-05-03 21:00:16 +0600

    Refactor star printing program for improved readability and update output format

--- reserver_star.c
+++ reserver_star.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     int n;
     scanf("%d", &n);
     int star = n;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j< star; j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < star; j++)
+        {
             printf("*");
         }
         star--;
