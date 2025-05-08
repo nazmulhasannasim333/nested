475e9618b5a0265f7f113e70ef36454605d77337
Nasim <mdnasimhosen333@gmail.com>
2025-05-08 23:56:42 +0600

    Update pattern5.c to include diamond pattern generation and modify output.txt for new formatting

--- pattern5.c
+++ pattern5.c
@@ -21,5 +21,23 @@ int main()
         space--;
     }
 
+    // create dymond pattern
+    space = 1;
+    val = n - 1;
+    for (int i = 1; i <= n - 1; i++)
+    {
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= val; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+        val--;
+        space++;
+    }
+
     return 0;
 }
